b39a4a7ff1e1ad29a3fc05e1cce0e4ff4ad639d7
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2024-09-10 22:52:57 -0300

    add database diagram

--- src/main/java/com/klan/otakuverse/OtakuverseApplication.java
+++ src/main/java/com/klan/otakuverse/OtakuverseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtakuverseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OtakuverseApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OtakuverseApplication.class, args);
+  }
 
 }
