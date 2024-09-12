cfb4a0f671913a96272d821bc2eb0a2362108bcd
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2024-09-12 14:05:15 -0300

    add docker-compose and flyway create a postgresql db

--- src/main/java/com/klan/otakuverse/OtakuverseApplication.java
+++ src/main/java/com/klan/otakuverse/OtakuverseApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtakuverseApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(OtakuverseApplication.class, args);
-  }
-
+	public static void main(String[] args) {
+		SpringApplication.run(OtakuverseApplication.class, args);
+	}
 }
